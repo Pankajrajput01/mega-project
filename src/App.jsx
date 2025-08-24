05f38d6be6ca2078c07e5f6734bce61b2f756216
Pankajrajput01 <pankajrathore8740@gmail.com>
2025-08-24 21:12:47 +0530

    create env

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import conf from './conf/conf'
+
+function App() {
+  console.log(conf);
+  
+  return (
+    <>
+      <h1>this is blog website</h1>
+    </>
+  )
+}
+
+export default App
