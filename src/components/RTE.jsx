14f95f8fed2718b8e1b9e2c7b9dc8f739c5285bb
Pankajrajput01 <pankajrathore8740@gmail.com>
2025-09-22 21:31:12 +0530

    update files

---
+++ src/components/RTE.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { Controller } from "react-hook-form";
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          {label}
+        </label>
+      )}
+      <Controller
+        name={name || "Content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Controller
+            name={name || "content"}
+            control={control}
+            render={({ field: { onChange } }) => (
+              <Editor
+                initialValue={defaultValue}
+                init={{
+                  initialValue: defaultValue,
+                  height: 500,
+                  menubar: true,
+                  plugins: [
+                    "image",
+                    "advlist",
+                    "autolink",
+                    "lists",
+                    "link",
+                    "image",
+                    "charmap",
+                    "preview",
+                    "anchor",
+                    "searchreplace",
+                    "visualblocks",
+                    "code",
+                    "fullscreen",
+                    "insertdatetime",
+                    "media",
+                    "table",
+                    "code",
+                    "help",
+                    "wordcount",
+                    "anchor",
+                  ],
+                  toolbar:
+                    "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                  content_style:
+                    "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                }}
+                onEditorChange={onChange}
+              />
+            )}
+          />
+        )}
+      />
+    </div>
+  );
+}
